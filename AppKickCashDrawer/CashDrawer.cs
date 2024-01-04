df865b77a19bcc9bdadea1b59a64e711ed0a8598
Minh Vu <minh.vu@floatingcube.com>
2024-01-04 14:03:57 +0700

    First Push

---
+++ AppKickCashDrawer/CashDrawer.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Drawer
+{
+    public class CashDrawer
+    {
+        public SerialPort ComPort;
+        private readonly string _name;
+
+        public static string GS = Convert.ToString((char)29);
+        public static string ESC = Convert.ToString((char)27);
+
+        #region Mechanism Control Command
+
+        //Kick Drawer
+        // Different depending on the type of printer, hardcode for now, for more visit http://keyhut.com/popopen.htm
+        public static string OPENDRAWER = ESC + Encoding.ASCII.GetString(new byte[] { 112, 0, 25, 250 });
+
+        public static string OPENDRAWERBYCOMPORT = Encoding.ASCII.GetString(new byte[] { 23, 112, 0, 25, 250 });
+
+        // DLE EOT 1
+        public static string CASHDRAWERSTATUS = Encoding.ASCII.GetString(new byte[] { 16, 4, 1 });
+
+        public static string CASHDRAWEROPENED = Encoding.ASCII.GetString(new byte[] { 50 });
+
+        public static string CASHDRAWERCLOSED = Encoding.ASCII.GetString(new byte[] { 54 });
+
+        #endregion Mechanism Control Command
+
+        #region Miscellaneous Command
+
+        public static string INITIALIZEPRINTER = ESC + "@";
+
+        #endregion Miscellaneous Command
+
+        public CashDrawer(string name)
+        {
+            _name = name;
+            OpenDeviceWithCOMPort(_name);
+        }
+
+        public bool OpenCashdrawer()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(_name)) return false;
+                var listCommandCashDrawer =
+                    new List<string>
+                    {
+                        OPENDRAWERBYCOMPORT,
+                        OPENDRAWER
+                    };
+                return OpenCashDrawer(listCommandCashDrawer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Open Cashd rawer Failed" + ex);
+                return false;
+            }
+        }
+
+        public bool IsCashdrawerOpening()
+        {
+            return GetCashdrawerStatus() == 0;
+        }
+
+        public void OpenDeviceWithCOMPort(string comPortName)
+        {
+            if (string.IsNullOrEmpty(comPortName)) return;
+
+            try
+            {
+                if (ComPort == null)
+                {
+                    ComPort = new SerialPort
+                    {
+                        PortName = comPortName,
+                        BaudRate = 9600,
+                        StopBits = StopBits.One,
+                        DataBits = 8,
+                        Parity = Parity.None
+                    };
+                }
+                else
+                {
+                    ComPort.PortName = comPortName;
+                }
+
+                ComPort?.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Open COM Port Failed: " + ex);
+            }
+        }
+
+        public void CloseDeviceWithCOMPort()
+        {
+            try
+            {
+                if (ComPort?.IsOpen == true) ComPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Close COM Port Failed: " + ex);
+            }
+            finally
+            {
+                ComPort.Dispose();
+            }
+        }
+
+        private int GetCashdrawerStatus()
+        {
+            if (string.IsNullOrEmpty(_name)) return -1;
+            return GetCashDrawerByComPortStatus();
+        }
+
+        private int GetCashDrawerByComPortStatus()
+        {
+            try
+            {
+                var messageBuilder = new StringBuilder();
+                messageBuilder.AppendLine("### Get Status Cash Drawer ###");
+                if (!ComPort.IsOpen) ComPort.Open();
+
+                if (ComPort.IsOpen)
+                {
+                    var cmd = CASHDRAWERSTATUS;
+                    messageBuilder.AppendLine("### Request ###");
+                    messageBuilder.AppendLine($"Details: '{cmd}'");
+                    messageBuilder.AppendLine();
+                    ComPort.WriteLine(cmd);
+                }
+
+                //Need to add deplay time to make sure can receive data from device
+                var taskDelay = Task.Run(async delegate
+                {
+                    await Task.Delay(100);
+                });
+                taskDelay.Wait();
+
+                messageBuilder.AppendLine("### Response ###");
+                var bytes = ComPort.BytesToRead;
+                messageBuilder.AppendLine($"BytesToRead: '{bytes}'");
+                var comBuffer = new byte[bytes];
+                messageBuilder.AppendLine($"ComBuffer: '{string.Join(", ", comBuffer)}'");
+                var readRespone = ComPort.Read(comBuffer, 0, bytes);
+                messageBuilder.AppendLine($"ComPortRead: '{readRespone}'");
+                var getString = Encoding.ASCII.GetString(comBuffer);
+                messageBuilder.AppendLine($"GetString: '{(getString == null ? "NULL" : getString == "" ? "EMPTY" : getString)}'");
+                MessageBox.Show(messageBuilder.ToString());
+                var cashDrawerStatus = getString?.Equals(CASHDRAWEROPENED) == true ? 0 : 1;
+                return cashDrawerStatus;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Get CashDrawer Status Failed" + ex);
+                return -1;
+            }
+            finally
+            {
+                CloseDeviceWithCOMPort();
+            }
+        }
+
+        private bool OpenCashDrawer(List<string> cmds)
+        {
+            try
+            {
+                if (ComPort == null) OpenDeviceWithCOMPort(_name);
+                if (ComPort.IsOpen == false) ComPort.Open();
+                if (ComPort.IsOpen)
+                {
+                    foreach (var cmd in cmds)
+                    {
+                        if (string.IsNullOrWhiteSpace(cmd)) continue;
+                        ComPort.WriteLine(cmd);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Open Cash Drawer Failed: " + ex);
+                return false;
+            }
+            finally
+            {
+                CloseDeviceWithCOMPort();
+            }
+        }
+    }
+}
---
+++ AppKickCashDrawer/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using Drawer;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace AppKickCashDrawer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CashDrawer _cashDrawer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComPortStatusTextBox.Text = "";
+            MessageTextBlock.Text = "";
+        }
+
+        private void OpenComPortButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var portName = ComPortTextBox.Text;
+                if (_cashDrawer == null)
+                {
+                    _cashDrawer = new CashDrawer(portName);
+                }
+                else
+                {
+                    _cashDrawer.CloseDeviceWithCOMPort();
+                    Thread.Sleep(10);
+                    _cashDrawer.OpenDeviceWithCOMPort(portName);
+                }
+
+                var isComPortOpened = _cashDrawer.ComPort.IsOpen;
+                if (isComPortOpened)
+                {
+                    OpenComPortButton.Content = $"ReOpen {portName}";
+                    ComPortStatusTextBox.Text = "OPENED";
+                    MessageTextBlock.Text = "";
+
+                    if (CloseComPortButton != null)
+                    {
+                        CloseComPortButton.Visibility = Visibility.Visible;
+                    }
+
+                    if (KickCashDrawerButton != null)
+                    {
+                        KickCashDrawerButton.Visibility = Visibility.Visible;
+                    }
+                }
+                else
+                {
+                    OpenComPortButton.Content = $"Open COM port";
+                    ComPortStatusTextBox.Text = "NOT OPENED";
+                    MessageTextBlock.Text = $"Open port {portName} failed!";
+
+                    if (CloseComPortButton != null)
+                    {
+                        CloseComPortButton.Visibility = Visibility.Collapsed;
+                    }
+
+                    if (KickCashDrawerButton != null)
+                    {
+                        KickCashDrawerButton.Visibility = Visibility.Collapsed;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void KickCashDrawerButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageTextBlock.Text = "";
+                if (_cashDrawer != null)
+                {
+                    var result = _cashDrawer?.OpenCashdrawer();
+                    if (result != null && result == true) {
+                        var isOpened = _cashDrawer.IsCashdrawerOpening();
+                        var message = "Cash drawer status: " + (isOpened ? "OPENNED" : "CLOSED");
+                        MessageTextBlock.Text = message;
+                    }
+                    else
+                    {
+                        MessageTextBlock.Text = "KICK FAILED";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CloseComPortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cashDrawer != null)
+            {
+                _cashDrawer.CloseDeviceWithCOMPort();
+                var isComPortOpened = _cashDrawer.ComPort.IsOpen;
+                var comPortStatus = isComPortOpened ? "OPENED" : "NOT OPENED";
+
+                CloseComPortButton.Visibility = isComPortOpened ? Visibility.Visible : Visibility.Collapsed;
+                KickCashDrawerButton.Visibility = isComPortOpened ? Visibility.Visible : Visibility.Collapsed;
+                ComPortStatusTextBox.Text = comPortStatus;
+                MessageTextBlock.Text = "";
+                OpenComPortButton.Content = $"Open COM port";
+            }
+        }
+
+        private void ComPortTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            _cashDrawer?.CloseDeviceWithCOMPort();
+
+            if (OpenComPortButton != null)
+            {
+                OpenComPortButton.Content = $"Open COM port";
+            }
+
+            if (ComPortStatusTextBox != null)
+            {
+                ComPortStatusTextBox.Text = "";
+            }
+
+            if (MessageTextBlock != null)
+            {
+                MessageTextBlock.Text = "";
+            }
+
+            if (CloseComPortButton != null)
+            {
+                CloseComPortButton.Visibility = Visibility.Collapsed;
+            }
+
+            if (KickCashDrawerButton != null)
+            {
+                KickCashDrawerButton.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
